5c99371ece60a9581f6c03e7d1c617b0d2a7cb7b
youxia <243802688@qq.com>
2024-02-23 14:38:10 +0800

    change export modules

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ export { FeiyunClient } from './client/FeiyunClient'
 export { GameSocketClient } from './client/GameSocketClient'
 
 export { Feiyun } from './server/feiyun'
+export { Server } from './server/server'
+export { Socket } from './server/socket'
 export { GameSocket } from './server/GameSocket'
 export { GameSocketServer } from './server/GameSocketServer'
 
