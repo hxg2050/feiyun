59c6d5557fde2c19e643ce06aabe5b937a26f104
youxia <243802688@qq.com>
2024-04-03 12:02:13 +0800

    fix: commonjs

--- packages/client/rollup.config.js
+++ packages/client/rollup.config.js
@@ -1,2 +1,17 @@
-import config from '../../rollup.config.mjs'
-export default config;
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript'
+import { defineConfig } from 'rollup'
+
+export default defineConfig({
+    input: 'src/index.ts',
+    output: [{
+        file: 'dist/index.cjs',
+        format: 'cjs',
+    }, {
+        file: 'dist/index.mjs',
+        format: 'esm',
+    }],
+    external: ['eventemitter3'],
+    plugins: [typescript(), commonjs(), nodeResolve()],
+})
